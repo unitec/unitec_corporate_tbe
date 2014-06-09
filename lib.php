ad8a0d4aeeacb9e8b5e7a268a5466e969266688f
Unitec eLearn <elearn@unitec.ac.nz>
2014-06-09 14:23:04 +1200

    completed

--- lib.php
+++ lib.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Unitec Corporate Theme
+ * Unitec TBE Theme
  *
  * @package   theme_unitec_corporate_tbe
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
